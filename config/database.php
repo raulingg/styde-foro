f8a9e1ab6c2d1f2db12ad3c0c08bbf2cd022fe44
Raul Quispe (raulingg) <robin_2004_89@hotmail.com>
2016-11-30 17:08:12 -0500

    Fix database configuration for CodeShip

--- config/database.php
+++ config/database.php
@@ -69,7 +69,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE_TESTS', 'foro_tests'),
+            'database' => env('DB_DATABASE_TESTS', 'styde_foro_tests'),
             'username' => env('MYSQL_USER' , env('DB_USERNAME', 'forge')),
             'password' => env('MYSQL_PASSWORD', env('DB_PASSWORD', '')),
             'charset' => 'utf8',
